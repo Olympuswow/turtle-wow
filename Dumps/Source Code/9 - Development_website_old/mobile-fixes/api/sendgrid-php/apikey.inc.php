04da926af01cb28776e0116398eeca719ce97bd7
RëXol <66186993+jorge990125@users.noreply.github.com>
2026-02-24 13:49:36 -0500

    Delete Dumps/Source Code/9 - Development_website_old/mobile-fixes directory

--- Dumps/Source Code/9 - Development_website_old/mobile-fixes/api/sendgrid-php/apikey.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-$sgapikey = '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';
-?>
--- Dumps/Source Code/9 - Development_website_old/mobile-fixes/password_success.php
+++
@@ -1,5 +0,0 @@
-<?php require_once('./header.php'); ?>
-<div class="turtle-main">
-<div class="turtle-block"><span>You've succesfully changed your password. Safe travels!</span></div>
-<?php require_once ('./footer.php') ?>
-</body></html>
